b5d84987edd2872c1c6b1cfd57d7c04aeda41bd9
Zablon18 <zablah.developer@gmail.com>
2019-08-08 19:10:59 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Card } from 'react-bootstrap';
+import fist from './fist.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ margin: 10 }}>
+      <Card style={{ width: '18rem', backgroundColor: '#FF3434', borderRadius: 10 }}>
+        <Card.Img src={fist} style={{ height: 35, width: 35, backgroundColor: 'white', borderRadius: 80, borderWidth: 1000 }} />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+    </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
